155811f7e2b654ce137c8bb2cc357ec1e27300f6
edissonal <edissonalonso@gmail.com>
2019-04-29 00:18:45 -0500

    Implementacion Modulo Boleteria

--- build/generated-sources/ap-source-output/entidades/Equipo2_.java
+++ build/generated-sources/ap-source-output/entidades/Equipo2_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-28T21:16:36")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-29T00:14:18")
 @StaticMetamodel(Equipo2.class)
 public class Equipo2_ { 
 
